562bb3d8e2fa6c8b614e6df8922a16e7d740d5c8
dexon03 <dima0958757803@gmail.com>
2024-02-06 11:53:49 +0200

    Init

---
+++ Auction.Api/Auction.Api/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using Auction.Core.Validation;
+using Auction.Infrastructure.Database;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auction.Api.Extensions;
+
+public static class DependencyInjectionExtension
+{
+    public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(opt =>
+            opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddValidation();
+        return services;
+    }
+}
---
+++ Auction.Api/Auction.Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auction.Infrastructure.Database;
+
+public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+{
+    
+}
